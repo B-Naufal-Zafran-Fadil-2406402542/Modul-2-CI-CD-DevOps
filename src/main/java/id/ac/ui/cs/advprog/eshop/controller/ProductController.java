64639b9cf451027c501bb32d9829b3362e43451e
nzafranf <nzfadhilll@gmail.com>
2026-02-26 15:51:13 +0700

    separated controller for Car and Product

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -1,9 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.controller;
 
-import id.ac.ui.cs.advprog.eshop.exception.ProductNotFoundException;
-import id.ac.ui.cs.advprog.eshop.model.Car;
+import id.ac.ui.cs.advprog.eshop.exception.ItemNotFoundException;
 import id.ac.ui.cs.advprog.eshop.model.Product;
-import id.ac.ui.cs.advprog.eshop.service.CarServiceImpl;
 import id.ac.ui.cs.advprog.eshop.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -29,7 +27,8 @@ public class ProductController {
 
     @PostMapping("/create")
     public String createProductPost(@ModelAttribute Product product, Model model) {
-        product.setProductId(UUID.randomUUID().toString());
+        // Why did I set the ID here?
+        product.setId(UUID.randomUUID().toString());
         service.create(product);
         return "redirect:list";
     }
@@ -44,11 +43,11 @@ public class ProductController {
     @GetMapping("/edit/{productId}")
     public String editProductPage(@PathVariable String productId, Model model) {
         try {
-            Product product = service.getProductById(productId);
+            Product product = service.getItemById(productId);
             model.addAttribute("product", product);
             return "editProduct";
         }
-        catch (ProductNotFoundException e) {
+        catch (ItemNotFoundException e) {
             return "redirect:/product/list";
         }
     }
@@ -62,72 +61,12 @@ public class ProductController {
     @PostMapping("/delete/{productId}")
     public String deleteProduct(@PathVariable String productId) {
         try {
-            service.delete(productId);
-        } catch (ProductNotFoundException e) {
+            service.deleteById(productId);
+        } catch (ItemNotFoundException e) {
             System.out.println("Delete failed: " + e.getMessage());
         }
         return "redirect:/product/list";
     }
 }
 
-@Controller
-@RequestMapping("/car")
-class CarController extends ProductController {
-    @Autowired
-    private CarServiceImpl carservice;
 
-    @GetMapping("/createCar")
-    public String createCarPage(Model model) {
-        Car car = new Car();
-        model.addAttribute("car", car);
-        return "createCar";
-    }
-
-    @PostMapping("/createCar")
-    public String createCarPost(@ModelAttribute Car car, Model model) {
-        carservice.create(car);
-        return "redirect:listCar";
-    }
-
-    @GetMapping("/listCar")
-    public String carListPage(Model model) {
-        List<Car> allCars = carservice.findAll();
-        model.addAttribute("cars", allCars);
-        return "carList";
-    }
-
-    @GetMapping("/editCar/{carId}")
-    public String editCarPage(@PathVariable String carId, Model model) {
-        Car car = carservice.findById(carId);
-        model.addAttribute("car", car);
-        return "editCar";
-    }
-
-    @PostMapping("/editCar")
-    public String editCarPost(@ModelAttribute Car car, Model model) {
-        carservice.update(car.getCarId(), car);
-        return "redirect:listCar";
-    }
-
-//    @PostMapping("/deleteCar")
-//    public String deleteCar(@RequestParam("carId") String carId) {
-//        carservice.deleteCarById(carId);
-//        return "redirect:listCar";
-//    }
-
-//    @PostMapping("/delete/{productId}")
-//    public String deleteProduct(@PathVariable String productId) {
-//        try {
-//            service.delete(productId);
-//        } catch (ProductNotFoundException e) {
-//            System.out.println("Delete failed: " + e.getMessage());
-//        }
-//        return "redirect:/product/list";
-//    }
-
-    @PostMapping("/deleteCar/{carId}")
-    public String deleteCar(@PathVariable String carId) {
-        carservice.deleteCarById(carId);
-        return "redirect:/car/listCar";
-    }
-}
