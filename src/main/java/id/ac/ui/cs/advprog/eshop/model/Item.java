68edb2ba4bc2e6ac18c472f51a837e4e109981fb
nzafranf <nzfadhilll@gmail.com>
2026-02-26 15:48:23 +0700

    abstract the Car and Product class into abstract class Item

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Item.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Item.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
-public class Product extends Item{
-
+public abstract class Item {
+    private String id;
+    private String name;
+    private int quantity;
 }
