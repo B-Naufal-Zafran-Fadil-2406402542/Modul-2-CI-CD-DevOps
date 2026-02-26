3dc52f72e885e95744c118bf10f6cde598b53fb6
nzafranf <nzfadhilll@gmail.com>
2026-02-26 15:51:38 +0700

    renamed exception to follow abstraction

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/exception/ItemNotFoundException.java
@@ -0,0 +1,8 @@
+package id.ac.ui.cs.advprog.eshop.exception;
+
+public class ItemNotFoundException extends RuntimeException{
+
+    public ItemNotFoundException(String message) {
+        super(message);
+    }
+}
